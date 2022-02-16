7b2244acb23cdbbd451c5a3620ab784f52e8320b
Ronald Siebes <ronald.siebes@gmail.com>
2022-02-16 08:42:43 +0000

    second change

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "This is the next change! \n"
-print "This is the second change! \n"
+print "This is the next change! \n";
+print "This is the second change! \n";
 ?>
